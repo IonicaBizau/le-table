48ee1c0502267ecdb865c112952fe98887a0e6d4
Ionică Bizău <bizauionica@yahoo.com>
2014-07-27 18:03:07 +0300

    Configurable marks

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ var LeTable = module.exports = function (options) {
     var self = this;
     self.data = [];
 
+
+    options = Object(options);
+    options.marks = Object(options.marks);
+
+
+    var marks = {};
+    for (var m in LeTable.defaults.marks) {
+        marks[m] = options.marks[m] || LeTable.defaults.marks[m];
+    }
+
     self.addRow = function (columns, ops) {
         self.data.push({
             d: columns
@@ -25,7 +35,26 @@ var LeTable = module.exports = function (options) {
         for (var i = 0; i < self.data.length; ++i) {
             var cRow = self.data[i]
             for (var ii = 0; ii < cRow.d.length; ++ii) {
-                var cell = Box("1x1", {
+                var cell = Box({
+                    w: 1
+                  , h: 1
+                  , marks: {
+                        nw: ((!i && !ii) ? marks.nw
+                            : (!i && ii < cRow.d.length) ? marks.mt
+                            : (!ii && i < self.data.length) ? marks.ml
+                            : marks.mm)
+                      , n:  marks.n
+                      , ne: (!i && ii === cRow.d.length - 1) ? marks.ne
+                            : marks.mr
+                      , e:  marks.e
+                      , se: marks.se
+                      , s:  marks.s
+                      , sw: (i === self.data.length - 1 && !ii) ? marks.sw
+                            : marks.mb
+                      , w:  marks.w
+                      , b: " "
+                    }
+                }, {
                     text: cRow.d[ii].toString()
                   , stretch: true
                   , autoEOL: true
--- test/index.js
+++ test/index.js
@@ -5,6 +5,23 @@ var data = [
   , ["Data 1.2", "Data 2.2"]
 ];
 
+Table.defaults.marks = {
+    nw: "┌"
+  , n:  "─"
+  , ne: "┐"
+  , e:  "│"
+  , se: "┘"
+  , s:  "─"
+  , sw: "└"
+  , w:  "│"
+  , b: " "
+  , mt: "┬"
+  , ml: "├"
+  , mr: "┤"
+  , mb: "┴"
+  , mm: "┼"
+};
+
 var myTable = new Table({});
 
 for (var i = 0; i < data.length; ++i) {
